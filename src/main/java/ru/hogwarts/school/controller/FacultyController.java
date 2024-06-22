818d9166e34116bed150486a6456245343bb7ac3
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-06-22 15:43:39 +0300

    task

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,73 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping(path = "faculties")
+public class FacultyController {
+
+    private final FacultyService service;
+
+    public FacultyController(FacultyService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Faculty> getFaculty(@PathVariable Long id) {
+        Faculty faculty = service.find(id);
+        if (faculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Faculty>> getAllFaculty() {
+        Collection<Faculty> faculty = service.getAllFaculty();
+        return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping(path = "/color/{color}")
+    public ResponseEntity<Collection<Faculty>> getAllFacultyByColor(@PathVariable String color) {
+        Collection<Faculty> faculty = service.getAllFacultyByColor(color);
+        return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping(path = "/searchTerm/{searchTerm}")
+    public ResponseEntity<List<Faculty>> getFacultyByNameOrColor(@PathVariable String searchTerm) {
+        List<Faculty> faculties = service.getFacultiesByNameOrColor(searchTerm);
+        if (!faculties.isEmpty()) {
+            return new ResponseEntity<>(faculties, OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public Faculty addFaculty(@RequestBody Faculty faculty) {
+        return service.add(faculty);
+    }
+
+    @PutMapping
+    public ResponseEntity<Faculty> updateFaculty(@RequestBody Faculty faculty) {
+        Faculty updated = service.edit(faculty);
+        if (updated != null) {
+            return new ResponseEntity<>(updated, OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFaculty(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,54 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty add(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty find(Long id) {
+        return facultyRepository.findById(id)
+                .orElse(null);
+    }
+
+    public Faculty edit(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public boolean delete(Long id) {
+        Optional<Faculty> facultyOptional = facultyRepository.findById(id);
+        if (facultyOptional.isPresent()) {
+            facultyRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Faculty> getAllFaculty() {
+        return facultyRepository.findAll();
+    }
+
+    public List<Faculty> getAllFacultyByColor(String color) {
+        return facultyRepository.findFacultiesByColor(color);
+    }
+
+    public List<Faculty> getFacultiesByNameOrColor(String searchTerm) {
+        return facultyRepository.findFacultiesByNameOrColorIgnoreCase(searchTerm);
+    }
+}
+
