eb3ead6ea99bfd402a5b241000b53af69cf9e16e
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-07-03 23:08:32 +0300

    homework test web app task 2

---
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -0,0 +1,101 @@
+package ru.hogwarts.school.controller;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class FacultyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FacultyRepository repository;
+
+    @SpyBean
+    private FacultyService service;
+
+    @InjectMocks
+    private FacultyController controller;
+    @Autowired
+    private FacultyRepository facultyRepository;
+
+    @Test
+    void getFaculty() {
+    }
+
+    @Test
+    void getAllFaculty() {
+    }
+
+    @Test
+    void getAllFacultyByColor() {
+    }
+
+    @Test
+    void getFacultyByNameOrColor() {
+    }
+
+    @Test
+    void addFaculty() throws Exception {
+        final Long id = 1L;
+        final String name = "gryffindor";
+        final String color = "red";
+
+        JSONObject facultyObject = new JSONObject();
+        facultyObject.put("name", name);
+        facultyObject.put("color", color);
+
+        Faculty faculty = new Faculty();
+        faculty.setName(name);
+        faculty.setId(id);
+        faculty.setColor(color);
+
+        when(facultyRepository.save(any(Faculty.class))).thenReturn(faculty);
+        when(facultyRepository.findById(any(Long.class))).thenReturn(Optional.of(faculty));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/faculties")
+                        .content(facultyObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.color").value(color));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/faculties/color/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.color").value(color));
+    }
+
+    @Test
+    void updateFaculty() {
+    }
+
+    @Test
+    void deleteFaculty() {
+    }
+}
---
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -0,0 +1,35 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentControllerTest {
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void getAllStudent() {
+    }
+
+    @Test
+    void getAllStudentByAge() {
+    }
+
+    @Test
+    void getAllStudentByAgeBetween() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void editStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
