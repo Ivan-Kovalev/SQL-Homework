04969f2e68c072fab2341184e4d283b0cb39527f
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-07-04 17:41:21 +0300

    homework test web app task 2 (v2.0)

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -6,10 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FacultyControllerTest {
@@ -51,6 +58,13 @@ class FacultyControllerTest {
 
     @Test
     void getFacultyByNameOrColor() throws Exception {
+        Faculty faculty = new Faculty();
+        faculty.setName("Gryffindor");
+        faculty.setColor("red");
+        faculty.setId(1L);
+
+        ResponseEntity<Faculty> facultyResponseEntity = restTemplate.postForEntity("/faculties", faculty, Faculty.class);
+
         Assertions
                 .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/faculties/searchTerm/red", List.class))
                 .isNotNull();
@@ -69,9 +83,42 @@ class FacultyControllerTest {
 
     @Test
     void updateFaculty() throws Exception {
+        Faculty faculty = new Faculty();
+        faculty.setName("Gryffindor");
+        faculty.setColor("red");
+        faculty.setId(1L);
+
+        ResponseEntity<Faculty> facultyResponseEntity = restTemplate.postForEntity("/faculties", faculty, Faculty.class);
+        Long savedFacultyId = requireNonNull(facultyResponseEntity.getBody()).getId();
+
+        Faculty facultyChanging = facultyResponseEntity.getBody();
+        facultyChanging.setName("Slytherin");
+        facultyChanging.setColor("green");
+        facultyChanging.setStudents(new ArrayList<>());
+
+        restTemplate.put("/faculties", facultyChanging);
+
+        ResponseEntity<Faculty> response = restTemplate.getForEntity("/faculties/" + savedFacultyId, Faculty.class);
+
+        assertEquals(response.getBody(), facultyChanging);
     }
 
     @Test
     void deleteFaculty() throws Exception {
+        Faculty faculty = new Faculty();
+        faculty.setName("Gryffindor");
+        faculty.setColor("red");
+        faculty.setId(1L);
+
+        ResponseEntity<Faculty> facultyResponseEntity = restTemplate.postForEntity("/faculties", faculty, Faculty.class);
+        Long savedFacultyId = requireNonNull(facultyResponseEntity.getBody()).getId();
+
+        ResponseEntity<Faculty> findResponse = restTemplate.getForEntity("/faculties/" + savedFacultyId, Faculty.class);
+        Assertions.assertThat(findResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        restTemplate.delete("/faculties/" + savedFacultyId);
+
+        ResponseEntity<Faculty> response = restTemplate.getForEntity("/faculties/" + savedFacultyId, Faculty.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -73,9 +77,41 @@ class StudentControllerTest {
 
     @Test
     void editStudent() {
+        Student student = new Student();
+        student.setName("Harry");
+        student.setAge(15);
+        student.setId(1L);
+
+        ResponseEntity<Student> studentResponseEntity = restTemplate.postForEntity("/students", student, Student.class);
+        Long savedStudentId = requireNonNull(studentResponseEntity.getBody()).getId();
+
+        Student studentChanging = studentResponseEntity.getBody();
+        studentChanging.setName("Harry Potter");
+        studentChanging.setAge(25);
+
+        restTemplate.put("/students", studentChanging);
+
+        ResponseEntity<Student> response = restTemplate.getForEntity("/students/" + savedStudentId, Student.class);
+
+        assertEquals(response.getBody(), studentChanging);
     }
 
     @Test
     void deleteStudent() {
+        Student student = new Student();
+        student.setName("Harry");
+        student.setAge(15);
+        student.setId(1L);
+
+        ResponseEntity<Student> studentResponseEntity = restTemplate.postForEntity("/students", student, Student.class);
+        Long savedStudentId = requireNonNull(studentResponseEntity.getBody()).getId();
+
+        ResponseEntity<Student> findResponse = restTemplate.getForEntity("/students/" + savedStudentId, Student.class);
+        Assertions.assertThat(findResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        restTemplate.delete("/students/" + savedStudentId);
+
+        ResponseEntity<Student> response = restTemplate.getForEntity("/students/" + savedStudentId, Student.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
